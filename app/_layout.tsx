4d2f71e785a7839be0fbc5946d3dfd234c407e61
Nut41tank <p.pruktaveesak@accenture.com>
2025-09-16 15:57:29 +0700

    add theme toggle

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { PaperProvider } from "react-native-paper";
+import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+
+  const paperTheme =
+    colorScheme === "dark"
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
   return (
-    <PaperProvider>
+    <PaperProvider theme={paperTheme}>
       <Stack>
         <Stack.Screen
           name="todo"
